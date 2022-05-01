9b67507bc50905753cad476e0da751be577d5ef3
Helder de Souza Cruvinel <heldercruvinel@gmail.com>
2022-05-01 00:53:27 -0300

    API Started

---
+++ src/main/java/com/helder/sentryapi/SentryapiApplication.java
@@ -0,0 +1,13 @@
+package com.helder.sentryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentryapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentryapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helder/sentryapi/SentryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helder.sentryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
